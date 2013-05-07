ca9fa58ddfe291480daf3438b1b22405a3ae747c
Jay Feldblum <y_feldblum@yahoo.com>
2013-05-07 17:11:37 -0400

    A better isqrt.

--- src/Juke.hs
+++ src/Juke.hs
@@ -265,4 +265,11 @@ itol b n = go [] b n where
 
 --  Integers
 isqrt :: Integral a => a -> a
-isqrt = floor . sqrt . fromIntegral
+isqrt 0 = 0
+isqrt 1 = 1
+isqrt n = head $ dropWhile (not . isRoot) iters where
+	twopows = iterate (^2) 2
+	(lowerRoot, lowerN) = last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
+	newtonStep x = div (x + div n x) 2
+	iters = iterate newtonStep (isqrt (div n lowerN) * lowerRoot)
+	isRoot r  =  r^2 <= n && n < (r+1)^2
